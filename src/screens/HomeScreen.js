7774421359e900544a6ee57444d8fd09d7e8218a
Sulaymon O'rinov <artessdu@gmail.com>
2025-06-11 15:20:47 +0500

    home screenga button qo'shildi

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ function HomeScreen(props) {
     return (
         <div>
             <h1>Hello Starscream</h1>
+            <button>Button ni bosganda karochchi nimadir bo'ladi</button>
         </div>
     );
 }
